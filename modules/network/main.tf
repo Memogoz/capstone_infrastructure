136609af917545691b4bea73bfaad78fc5e989f7
Memogoz <guillermoglez583@gmail.com>
2025-08-26 09:55:45 -0600

    feat: refine network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "private_b" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, 2) # Changed to avoid CIDR overlap
-  availability_zone       = var.az_b
+  availability_zone       = var.az_a
   map_public_ip_on_launch = true
   tags                    = { Name = "${var.prefix}-public-${var.az_b}" }
 }
@@ -32,26 +32,31 @@ resource "aws_internet_gateway" "igw" {
   tags   = { Name = "${var.prefix}-igw" }
 }
 
-resource "aws_route_table" "public" {
+resource "aws_route_table" "vpc_table" {
   vpc_id = aws_vpc.this.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-  tags = { Name = "${var.prefix}-public-rt" }
+  tags = { Name = "${var.prefix}-private-rt" }
 }
 
 resource "aws_route_table_association" "private_a" {
   subnet_id      = aws_subnet.private_a.id # Corrected typo
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.vpc_table.id
 }
 
 resource "aws_route_table_association" "private_b" {
   subnet_id      = aws_subnet.private_b.id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.vpc_table.id
 }
 
-resource "aws_security_group" "alb_sg" {
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet
+  route_table_id = aws_route_table.vpc_table.id
+}
+
+resource "aws_security_group" "alb_sg" {  # Allow HTTP and allowed IPs to the load balancer
   name        = "${var.prefix}-alb-sg"
   description = "Allow HTTP from allowed CIDRs"
   vpc_id      = aws_vpc.this.id
@@ -70,16 +75,27 @@ resource "aws_security_group" "alb_sg" {
   tags = { Name = "${var.prefix}-alb-sg" }
 }
 
-resource "aws_security_group" "web_sg" {
+resource "aws_security_group" "web_sg" { # Allow HTTP from ALB and SSH from admin nodes to web servers
   name        = "${var.prefix}-web-sg"
-  description = "Allow HTTP from allowed CIDRs and from ALB"
+  description = "Allow HTTP from ALB and SSH from admin nodes"
   vpc_id      = aws_vpc.this.id
+
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_cidrs
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+    description     = "Allow HTTP from ALB"
   }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.admin_nodes_sg.id]
+    description     = "Allow SSH from admin nodes"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -89,22 +105,11 @@ resource "aws_security_group" "web_sg" {
   tags = { Name = "${var.prefix}-web-sg" }
 }
 
-resource "aws_security_group_rule" "allow_http_from_alb" {
-  type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.web_sg.id
-  source_security_group_id = aws_security_group.alb_sg.id
-  description              = "Allow HTTP from ALB"
-}
-
-resource "aws_security_group" "db_sg" {
+resource "aws_security_group" "db_sg" {  # Allow PostgreSQL traffic to web servers
   name        = "${var.prefix}-db-sg"
   description = "Allow PostgreSQL traffic from web servers"
   vpc_id      = aws_vpc.this.id
 
-  # Allow inbound PostgreSQL traffic from the web security group
   ingress {
     from_port       = 5432
     to_port         = 5432
@@ -123,7 +128,7 @@ resource "aws_security_group" "db_sg" {
   tags = { Name = "${var.prefix}-db-sg" }
 }
 
-resource "aws_security_group" "admin_sg" {
+resource "aws_security_group" "admin_nodes_sg" {  # Allow SSH to admin nodes
   name        = "${var.prefix}-builder-sg"
   description = "Allow HTTP from allowed CIDRs"
   vpc_id      = aws_vpc.this.id
--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -6,6 +6,10 @@ output "private_subnet_ids" {
   value = [aws_subnet.private_a.id, aws_subnet.private_b.id]
 }
 
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
 output "alb_sg_id" {
   value = aws_security_group.alb_sg.id
 }
@@ -16,4 +20,8 @@ output "web_sg_id" {
 
 output "db_sg_id" {
   value = aws_security_group.db_sg.id
+}
+
+output "admin_nodes_sg_id" {
+  value = aws_security_group.admin_nodes_sg.id
 }
