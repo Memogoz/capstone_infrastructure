1b5233d780f1d3b6feae4797f7253200519d855b
Memogoz <guillermoglez583@gmail.com>
2025-09-04 11:55:40 -0600

    feat: add iam role for web instances

---
+++ modules/iam/main.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_role" "ec2_web_role" {
+  name = "ec2-web-ecr-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecr_pull_policy" {
+  name        = "EC2ECRPullPolicy"
+  description = "Allow EC2 instances to authenticate to ECR and pull images"
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ],
+        Resource = var.ecr_arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ecr_policy" {
+  role       = aws_iam_role.ec2_web_role.name
+  policy_arn = aws_iam_policy.ecr_pull_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_web_instance_profile" {
+  name = "ec2-web-instance-profile"
+  role = aws_iam_role.ec2_web_role.name
+}
---
+++ modules/iam/outputs.tf
@@ -0,0 +1,3 @@
+output "web_instance_profile_name" {
+    value = aws_iam_instance_profile.ec2_web_instance_profile.name  
+}
